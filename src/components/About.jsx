275a0d889dc64ed00aaaa6db47bd53cf35fb3f9a
Vashu-Nayak <nvashu1@gmail.com>
2024-07-29 00:49:47 +0530

    animation commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,22 +2,45 @@ import React from 'react'
 import aboutImg from "../assets/resumephoto.png";
 import { ABOUT_TEXT } from '../constants';
 import { motion } from 'framer-motion'
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+            delay: 0.5
+  
+        }
+    }
+})
 
 const About = () => {
     return (
-        <div className='border-b border-neutral-900 pb-4'>
-            <h1 className="my-20 text-center text-4xl">
+        <div className='border-b border-neutral-900 pb-2'>
+            <h1 
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1.5 }}
+            className="my-20 text-center text-4xl"
+            >
                 About
                 <span className="text-neutral-500"> Me</span>
             </h1>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap  items-center">
                 <motion.div
                     whileInView={{ opacity: 1, x: 0 }}
                     initial={{ opacity: 0, x: -100 }}
                     transition={{ duration: 0.5 }}
                     className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
-                        <img className="rounded-2xl" src={aboutImg} alt="about" />
+                        <motion.img 
+                        variants={iconVariants(2)}
+                        initial="initial"
+                        animate="animate"
+                        className="rounded-2xl" src={aboutImg} alt="about" />
                     </div>
                 </motion.div>
                 <motion.div
@@ -25,7 +48,7 @@ const About = () => {
                     initial={{ opacity: 0, x: 100 }}
                     transition={{ duration: 0.5 }}
                     className="w-full lg:w-1/2">
-                    <div className="flex justify-center lg:justify-start">
+                    <div className="flex justify-center pt-20 lg:justify-start">
                         <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
                     </div>
                 </motion.div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -30,7 +30,9 @@ const Technologies = () => {
                 initial={{ opacity: 0, x: -100 }}
                 transition={{ duration: 1.5 }}
                 className="my-20 text-center text-4xl"
-            >Technologies</motion.h1>
+            >
+                Technologies
+            </motion.h1>
             <motion.div
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
